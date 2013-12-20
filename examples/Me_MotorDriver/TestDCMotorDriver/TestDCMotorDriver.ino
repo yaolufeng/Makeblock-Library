a6140c4b46efa5af6576ed1093ceaf6ca44a3824
indream5257 <flashindream@gmail.com>
2013-12-20 10:16:07 +0800

    path changed

--- examples/Me_MotorDriver/TestDCMotorDriver/TestDCMotorDriver.ino
+++ examples/Me_MotorDriver/TestDCMotorDriver/TestDCMotorDriver.ino
@@ -5,7 +5,7 @@
 * Version            : V1.0.1
 * Date               : 11/14/2013
 * Description        : Test for Makeblock Electronic modules of Me -Motor 
-                       Driver. You can directly connect a motor to M1 or M2 Port, or connect to PORT_1 or PORT_2 through a DC motor driver
+                       Driver. You can directly connect a motor to M1 or M2 Port, or connect to PORT_1 or PORT_2 through a DC                        motor driver
 * License            : CC-BY-SA 3.0
 * Copyright (C) 2013 Maker Works Technology Co., Ltd. All right reserved.
 * http://www.makeblock.cc/
@@ -31,22 +31,17 @@ void loop()
 {
 	motor1.run(motorSpeed); // value: between -255 and 255.
 	motor2.run(motorSpeed); // value: between -255 and 255.
-    motor3.run(motorSpeed);
-    motor4.run(motorSpeed);
+        motor3.run(motorSpeed);
+        motor4.run(motorSpeed);
 	delay(2000);
-	motor1.stop();
-	motor2.stop();
-    motor3.stop();
-	motor4.stop();
-	delay(100);
 	motor1.run(-motorSpeed);
 	motor2.run(-motorSpeed);
-    motor3.run(-motorSpeed);
-    motor4.run(-motorSpeed);
+        motor3.run(-motorSpeed);
+        motor4.run(-motorSpeed);
 	delay(2000);
 	motor1.stop();
 	motor2.stop();
-    motor3.stop();
+        motor3.stop();
 	motor4.stop();
 	delay(2000);
 }
--- examples/Me_RJ11Adapter/TestRJ11Adapter/TestRJ11Adapter.ino
+++ examples/Me_RJ11Adapter/TestRJ11Adapter/TestRJ11Adapter.ino
@@ -1,9 +1,9 @@
 /*************************************************************************
 * File Name          : TestRJ11Adapter.ino
 * Author             : Steve
-* Updated            : Xiaoyu
+* Updated            : Steve
 * Version            : V1.0.0
-* Date               : 12/13/2013
+* Date               : 2/22/2013
 * Description        : Test for Makeblock Electronic modules of Me - 
                        RJ11 Adapter. The module can ONLY be connected 
                        to the PORT_3, PORT_4, PORT_5, PORT_6, PORT_7, 
@@ -17,7 +17,7 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 
-MePort output(PORT_4); 
+MeOutput output(PORT_4); 
                                     
 void setup()
 {
@@ -27,8 +27,8 @@ void setup()
 
 void loop()
 {
-	output.Dwrite1(HIGH);
-	delay(1000);
-        output.Dwrite1(LOW);
-        delay(1000);
+	output.write1(1023);
+	delay(50);
+        output.write1(0);
+        delay(50);
 }
