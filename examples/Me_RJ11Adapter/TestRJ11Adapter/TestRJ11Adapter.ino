e64f41288722b0f8af3278b5d9a96e4c27026193
indream5257 <flashindream@gmail.com>
2013-12-31 15:17:28 +0800

    fix old class

--- examples/Me_RJ11Adapter/TestRJ11Adapter/TestRJ11Adapter.ino
+++ examples/Me_RJ11Adapter/TestRJ11Adapter/TestRJ11Adapter.ino
@@ -1,9 +1,9 @@
 /*************************************************************************
 * File Name          : TestRJ11Adapter.ino
 * Author             : Steve
-* Updated            : Steve
-* Version            : V1.0.0
-* Date               : 2/22/2013
+* Updated            : Ander
+* Version            : V1.0.1
+* Date               : 12/22/2013
 * Description        : Test for Makeblock Electronic modules of Me - 
                        RJ11 Adapter. The module can ONLY be connected 
                        to the PORT_3, PORT_4, PORT_5, PORT_6, PORT_7, 
@@ -16,19 +16,15 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
-
-MeOutput output(PORT_4); 
-                                    
+MePort output(PORT_4);                            
 void setup()
 {
 	Serial.begin(9600);
-
 }
-
 void loop()
 {
-	output.write1(1023);
+	output.Awrite1(1023);
 	delay(50);
-        output.write1(0);
+        output.Awrite1(0);
         delay(50);
 }
