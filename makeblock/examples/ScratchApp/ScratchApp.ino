a3341733d4a369131e2cbd2e955a49135cc77dc3
ander <ander@makeblock.cc>
2014-03-19 17:23:10 +0800

    update

--- makeblock/examples/ScratchApp/ScratchApp.ino
+++ makeblock/examples/ScratchApp/ScratchApp.ino
@@ -1,85 +1,270 @@
+/*************************************************************************
+* File Name          : ScratchApp.ino
+* Author             : Ander
+* Updated            : Ander
+* Version            : V1.0.0
+* Date               : 03/19/2014
+* Description        : Makeblock Electronic for  Scratch's Communication
+* License            : CC-BY-SA 3.0
+* Copyright (C) 2014 Maker Works Technology Co., Ltd. All right reserved.
+* http://www.makeblock.cc/
+**************************************************************************/
 #include "Makeblock.h"
 #include <SoftwareSerial.h>
 #include <Wire.h>
 MeSerial serial;
-MeDCMotor motor1(M1);
-MeDCMotor motor2(M2);
-MeDCMotor motor3(PORT_1);
-MeDCMotor motor4(PORT_2);
-MeUltrasonicSensor us;
-MeLimitSwitch ls;
+MeDCMotor motor;
+MeServo servo;
+MeUltrasonicSensor ultrasonic;
+MeLineFinder linefinder;
+MeLimitSwitch limitswitch;
+MeJoystick joystick;
+MeSoundSensor soundsensor;
+MeLightSensor lightsensor;
+Me4Button button;
+MeTemperature temperature;
 char *device;
 char *method;
 int port;
+char *pin;
 int value;
 int slot;
 int ultrasonic_port=-1;
 int switch_port=-1;
 int switch_slot=1;
-
-void setup() {
-  serial.begin(9600);
-  while(!Serial){
+long servoTime = 0;
+bool isServoRun = false;
+typedef struct{
+  int type;
+  int mode;
+  int slot[2];
+}Module;
+Module ports[10];
+Module pins[18];
+int analogPins[6] = {A0,A1,A2,A3,A4,A5};
+void addDevice(){
+  if(port>0){
+    int index = port-1;
+    if(strcmp(device,"motor")==0){
+      ports[index].type=1;
+    }else if(strcmp(device,"servo")==0){
+      ports[index].type=2;
+      ports[index].slot[slot-1]=1;
+    }else if(strcmp(device,"Ultrasonic")==0){
+      ports[index].type=3;
+    }else if(strcmp(device,"Line%20Finder")==0){
+      ports[index].type=4;
+    }else if(strcmp(device,"LimitSwitch")==0){
+      ports[index].type=5;
+      ports[index].slot[slot-1]=1;
+    }else if(strcmp(device,"Light%20Sensor")==0){
+      ports[index].type=6;
+    }else if(strcmp(device,"Sound%20Sensor")==0){
+      ports[index].type=7;
+    }else if(strcmp(device,"Temperature")==0){
+      ports[index].type=8;
+      ports[index].slot[slot-1]=1;
+    }else if(strcmp(device,"Joystick")==0){
+      ports[index].type=9;
+    }else if(strcmp(device,"Button")==0){
+      ports[index].type=10;
+    }
   }
-  serial.println("application start");
+}
+void runDevice(){
+  if(port>0){
+    int index = port-1;
+    Module portModule = ports[index];
+    if(strcmp(device,"motor")==0){
+      motor.reset(port);
+      motor.run(value);
+    }else if(strcmp(device,"servo")==0){
+      servo.reset(port,slot);
+      servo.begin();
+      servo.write(value);
+      isServoRun = true;
+      servoTime = millis();
+    }
+  }
+}
+void checkDevice(){
+  int i,portNum;
+  for(i=0;i<10;i++){
+    portNum = i+1;
+    if(ports[i].type>0){
+      switch(ports[i].type){
+        case 3:{
+          ultrasonic.reset(portNum);
+          int dist = ultrasonic.distanceCm();
+          sendCommand("Ultrasonic/Port",portNum,-1,dist);
+        }
+        break;
+        case 4:{
+          linefinder.reset(portNum);
+          int state = linefinder.readSensors();
+          sendCommand("LineFinder/Port",portNum,-1,state);
+        }
+        break;
+        case 5:{
+          limitswitch.reset(portNum,ports[i].slot[0]==1?1:2);
+          int touched = limitswitch.touched();
+          sendCommand("LimitSwitch/Port",portNum,ports[i].slot[0]==1?1:2,touched==0,true);
+        }
+        break;
+        case 6:{
+          lightsensor.reset(portNum);
+          int light = lightsensor.strength();
+          sendCommand("LightSensor/Port",portNum,-1,light);
+        }
+        break;
+        case 7:{
+          soundsensor.reset(portNum);
+          int sound = soundsensor.strength();
+          sendCommand("SoundSensor/Port",portNum,-1,sound);
+        }
+        break;
+        case 8:{
+          temperature.reset(portNum,ports[i].slot[0]==1?1:2);
+          float t = temperature.temperature();
+          if(t==-100){
+            t = temperature.temperature();
+          }
+          if(t>-100){
+            sendCommand("Temperature/Port",portNum,ports[i].slot[0]==1?1:2,t);
+          }
+        }
+        break;
+        case 9:{
+          joystick.reset(portNum);
+          int dist = joystick.readX();
+          serial.print("Joystick/Port");
+          serial.print(portNum);
+          serial.print(ports[i].slot[0]==1?"/X-Axis":"/Y-Axis");
+          serial.print(" ");
+          serial.println(dist);
+        }
+        break;
+        case 10:{
+          button.reset(portNum);
+          int state = button.pressed();
+          sendCommand("Button/Port",portNum,-1,state);
+        }
+        break;
+      }
+    }
+  }
+  for(i=0;i<18;i++){
+    if(pins[i].type==1){
+      sendCommand("digital/D",i+2,-1,digitalRead(i+2));
+    }else if(pins[i].type==2){
+      sendCommand("analog/A",i-12,-1,analogRead(analogPins[i-12]));
+    }
+  }
+  if(isServoRun&&millis()-servoTime>500){
+     isServoRun = false;
+     servo.detach(); 
+  }
+}
+void sendCommand(const char*cmd,int cPort,int cSlot,double v){
+  sendCommand(cmd,cPort,cSlot,v,false);
+}
+void sendCommand(const char*cmd,int cPort,int cSlot,double v,bool isBool){
+  serial.print(cmd);
+  serial.print(cPort);
+  if(cSlot>-1){
+    serial.print("/Slot");
+    serial.print(cSlot);
+  }
+  serial.print(" ");
+  if(isBool){
+    serial.println(v!=0?"true":"false");
+  }else{
+    serial.println(v);
+  }
+}
+void setPinMode(){
+  if(value==0){
+    return;
+  }
+  if(pin[0]=='D'){
+    pin[0]='0';
+    int p = atoi(pin);
+    pinMode(p,value==2?OUTPUT:INPUT);
+    if(value==1){
+      pins[p-2].type = 1;
+    }else{
+      pins[p-2].type = 0;
+    }
+  }else if(pin[0]=='A'){
+    pin[0]='0';
+    int p = atoi(pin);
+    pinMode(analogPins[p],value==2?OUTPUT:INPUT);
+    if(value==1){
+      pins[p+12].type = 2;
+    }else{
+      pins[p+12].type = 0;
+    }
+  }
+}
+void pinWrite(){
+  if(pin[0]=='D'){
+    pin[0]='0';
+    digitalWrite(atoi(pin),value==2?HIGH:LOW);
+  }else if(pin[0]=='A'){
+    pin[0]='0';
+    digitalWrite(analogPins[atoi(pin)],value==2?HIGH:LOW);
+  }
+}
+
+long currentTime = 0;
+long sampleTime = 100;
+long baudrate = 115200;
+void setup() {
+  serial.begin(baudrate);
+  serial.println("Application Start");
 }
 void loop() {
+  long time = millis()-currentTime;
+  if(time>sampleTime||time<0){
+    currentTime = millis();
+    checkDevice();
+  }
+  
   if(serial.paramAvailable()){
-    
-    device=serial.getParamCode("device");
+    device = serial.getParamCode("device");
     port = serial.getParamValue("port");
+    pin = serial.getParamCode("pin");
     value = serial.getParamValue("value");
     slot = serial.getParamValue("slot");
     method = serial.getParamCode("method");
-    if(strcmp(device,"motor")==0){
-      switch(port){
-        case M1:{
-         motor1.run(value); 
-        }
-        break;
-        case M2:{
-         motor2.run(value); 
-        }
-        break;
-        case PORT_1:{
-         motor3.run(value); 
-        }
-        break;
-        case PORT_2:{
-         motor4.run(value); 
-        }
-        break;
+    
+    if(strcmp(device,"reset")==0){
+      int i;
+      for(i=0;i<10;i++){
+         ports[i].type = 0;
+         ports[i].slot[0] = 0;
+         ports[i].slot[1] = 0;
       }
+      for(i=0;i<18;i++){
+         pins[i].type = 0;
+         pins[i].slot[0] = 0;
+         pins[i].slot[1] = 0;
+      }
+      motor.stop();
+      return;
     }
-    if(strcmp(device,"Ultrasonic")==0){
-      if(strcmp(method,"add")==0){
-        
-        ultrasonic_port = port;
-        us.reset(port);
-      }
-    }
-    if(strcmp(device,"LimitSwitch")==0){
-      if(strcmp(method,"add")==0){
-          switch_port = port;
-          switch_slot = slot;
-          ls.reset(port,slot);
-      }
-    }
-    if(strcmp(device,"poll")==0){
-      if(ultrasonic_port>-1){
-        serial.print("Ultrasonic/Port");
-        serial.print(ultrasonic_port);
-        serial.print(" ");
-        serial.println(us.distanceCm());
-      } 
-      if(switch_port>-1){
-        serial.print("LimitSwitch/Port");
-        serial.print(switch_port);
-        serial.print("/Slot");
-        serial.print(switch_slot);
-        serial.print(" ");
-        serial.println(ls.touched()?"false":"true");
-      }
+    if(strcmp(method,"add")==0){
+     addDevice(); 
+     return;
+    }else if(strcmp(method,"run")==0){
+     runDevice(); 
+     return;
+    }else if(strcmp(method,"mode")==0){
+     setPinMode(); 
+     return;
+    }else if(strcmp(method,"write")==0){
+     pinWrite(); 
+     return;
     }
   }
 }
